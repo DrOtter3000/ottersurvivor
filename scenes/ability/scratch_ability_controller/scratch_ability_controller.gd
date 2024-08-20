a2b658b9ba5775406ccc67528c3d6202c9e9d036
DrOtter3000 <drotter3000@fedora.fritz.box>
2024-08-20 12:29:18 +0200

    make scratch choose nearest enemy

--- scenes/ability/scratch_ability_controller/scratch_ability_controller.gd
+++ scenes/ability/scratch_ability_controller/scratch_ability_controller.gd
@@ -1,13 +1,29 @@
 extends Node
 
+const MAX_RANGE = 150
+
 @export var scratch_ability: PackedScene
 
-
 func _on_timer_timeout() -> void:
-	var player = get_tree().get_first_node_in_group("player") as Node2D
+	var player = get_tree().get_first_node_in_group("player")
 	if player == null:
 		return
 	
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+	
+	if enemies.size() == 0:
+		return
+	
+	enemies.sort_custom(func(a: Node2D, b: Node2D):
+		var a_distance = a.global_position.distance_squared_to(player.global_position)
+		var b_distance = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+	
+
 	var scratch_instance = scratch_ability.instantiate() as Node2D
 	player.get_parent().add_child(scratch_instance)
-	scratch_instance.global_position = player.global_position
+	scratch_instance.global_position = enemies[0].global_position
