94ccaf7f606e054422fcf6ce0e410f797e3db225
DrOtter3000 <drotter3000@fedora.fritz.box>
2024-08-21 11:14:35 +0200

    add enemy manager

---
+++ scenes/manager/enemy_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const SPAWN_RADIUS = 400
+
+@export var duck_scene: PackedScene
+
+
+func _on_timer_timeout() -> void:
+	var player = get_tree().get_first_node_in_group("player")
+	if player == null:
+		return
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+	var enemy = duck_scene.instantiate() as Node2D
+	get_parent().add_child(enemy)
+	enemy.global_position = spawn_position
