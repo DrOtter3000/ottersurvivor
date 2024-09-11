b8b0dad2ecbc34dd095d6b5c489ba0d5681aba3f
DrOtter3000 <info@darknet-online.com>
2024-09-11 12:03:58 +0200

    animate clam collection

--- scenes/game_object/clam/clam.gd
+++ scenes/game_object/clam/clam.gd
@@ -1,6 +1,24 @@
 extends Node2D
 
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _ready() -> void:
+	rotation_degrees = randi_range(0, 360)
+
+
+func tween_collect(percent: float, start_position: Vector2):
+	var player = get_tree().get_first_node_in_group("player")
+	if player == null:
+		return
+	
+	global_position = start_position.lerp(player.global_position, percent)
+
+
+func collect():
 	GameEvents.emit_clam_collected(1)
 	queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	var tween = create_tween()
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, .5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_EXPO)
+	tween.tween_callback(collect)
