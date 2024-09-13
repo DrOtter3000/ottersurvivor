10ecf3d007c9844e3644efda5c8c5a556acdda73
DrOtter3000 <info@darknet-online.com>
2024-09-13 12:12:53 +0200

    add hit flash

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -23,5 +23,5 @@ func on_area_entered(other_area: Area2D):
 	var floating_text = floating_text_scene.instantiate() as Node2D
 	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
 	
-	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.global_position = global_position + (		Vector2.UP * 16)
 	floating_text.start(str(hitbox_component.damage))
