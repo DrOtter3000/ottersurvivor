55a06752da770476dcc06ed29333f88bbc7bf50d
DrOtter3000 <info@darknet-online.com>
2024-10-29 20:41:28 +0100

    animate upgrade cards

--- scenes/UI/ability_upgrade_card.gd
+++ scenes/UI/ability_upgrade_card.gd
@@ -4,7 +4,33 @@ signal selected
 
 @onready var name_label: Label = $MarginContainer/VBoxContainer/PanelContainer/NameLabel
 @onready var description_label: Label = $MarginContainer/VBoxContainer/DescriptionLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hover_animation_player: AnimationPlayer = $HoverAnimationPlayer
 
+var disabled := false
+
+
+func play_in(delay: float = 0) -> void:
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	animation_player.play("in")
+
+
+func play_discard() -> void:
+	animation_player.play("discard")
+
+
+func select_card() -> void:
+	disabled = true
+	animation_player.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		other_card.play_discard()
+	
+	await animation_player.animation_finished
+	selected.emit()
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
@@ -12,5 +38,15 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 
 
 func _on_gui_input(event: InputEvent) -> void:
+	if disabled:
+		return
+	
 	if event.is_action_pressed("LMB"):
-		selected.emit()
+		select_card()
+
+
+func _on_mouse_entered() -> void:
+	if disabled:
+		return
+	
+	hover_animation_player.play("hover")
