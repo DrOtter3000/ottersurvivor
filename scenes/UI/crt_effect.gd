d95189d052e2abbf992417229bffa83333ae377b
DrOtter3000 <info@darknet-online.com>
2024-10-30 19:52:24 +0100

    add hurt screen

--- scenes/UI/crt_effect.gd
+++ scenes/UI/crt_effect.gd
@@ -1,10 +1,1 @@
 extends CanvasLayer
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	GameEvents.player_damaged.connect(_on_player_damaged)
-
-
-func _on_player_damaged():
-	pass
