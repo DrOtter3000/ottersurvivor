43e3be782c23eb0f1939e41c5475bbda9316daea
DrOtter3000 <info@darknet-online.com>
2024-10-30 20:48:38 +0100

    make randomized pitch optional

--- scenes/component/random_stream_player_2d_component.gd
+++ scenes/component/random_stream_player_2d_component.gd
@@ -2,13 +2,14 @@ extends AudioStreamPlayer2D
 
 
 @export var streams: Array[AudioStream]
-
+@export var randomized_pitch := true
 
 func play_random():
 	if streams == null || streams.size() == 0:
 		return
-	
-	pitch_scale = randf_range(.9, 1.1)
+		
+	if randomized_pitch:
+		pitch_scale = randf_range(.9, 1.1)
 	
 	stream = streams.pick_random()
 	
