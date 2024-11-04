2850e8a8568c3d41f96bec5935eac71da9ae5169
DrOtter3000 <info@darknet-online.com>
2024-11-04 18:45:25 +0100

    add options menu

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -1,12 +1,22 @@
 extends CanvasLayer
 
+var options_scene = preload("res://scenes/UI/options_menu.tscn")
+@onready var quit_button: Button = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/QuitButton
+
+
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 
 func _on_options_button_pressed() -> void:
-	pass
+	var options_instance = options_scene.instantiate()
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_options_closed.bind(options_instance))
+
+
+func on_options_closed(options_instance: Node):
+	options_instance.queue_free()
 
 
 func _on_quit_button_pressed() -> void:
